71cb0a2ca8ec6a6171ebc5cdbb96ce06b7842240
Timmy Wu <timmy@stringbean.tech>
2019-11-20 20:31:02 -0500

    feat: add a few function

--- main/contracts/RWRC.sol
+++ main/contracts/RWRC.sol
@@ -2,36 +2,72 @@ pragma solidity ^0.5.11;
 
 contract RWRC{
 
-	enum State { Unadopted, Waiting_for_verifier, Verified, Completed}
+	enum State {Unadopted, Adopted, Waiting_for_verifier, Verified, Not_Verified, Completed};
+	State default_state = State.Unadopted;
+    enum Level {High, Low};
+	Level level = Level.Low; 
 	address owner;
 	uint256 tid;
-	// deadline 
-	uint256 price;
-	uint256 stake; 
-	uint256 verifiers;
-	address[] verifiersList;
+	// deadline
+	uint256 stake;
+	address verifier;
+	address worker;
+	string description;
+	string answer;
 
-	constructor(uint256 taskid, uint256 number_of_verifiers, uint256 pay, uint256 stake_amount) public {
+
+	constructor(uint256 taskid, uint256 dif_level, uint256 stake_amount, string memory des) public {
 		tid = taskid;
 		owner = msg.sender;
-		price = pay;
-		stake = stake_amount; 
-		verifiers = number_of_verifiers;
+		stake = stake_amount;
+		description = des;
+
+		if (dif_level == 1){
+			level = Level.High;
+		} else {
+			level = Level.Low; 
+		}
 	}
 
-	// When a verfier verifies a task, he is added to a list, once the length of the list of addresses of verifiers
-	// corresponds to the required number of verifiers explicitly asked by the requester
+	// worker adopt a task
+	// TODO: interfacing with registration contract
+	function worker_adopt() public returns(bool){
+		require(owner != msg.sender, "error: requester as worker");
 
-	function add_verifier() public returns(bool){
+		// add worker address
+		worker = msg.sender;
+		default_state = State.Adopted;
+		return true;
+	}
 
-		for (uint i = 0; i < verifiersList.length; i++){
-			if (verifiersList[i] == msg.sender){
-				return false;
-			}
+	function complete_task(string memory ans) public returns(bool){
+		require(owner != msg.sender, "error: requester as worker");
+
+		// worker has completed task
+		default_state = State.Waiting_for_verifier;
+		answer = ans;
+		return true;
+	}
+
+	function verify_task(bool approve_choice) public returns(bool){
+		require(msg.sender != worker, "error: worker as requester");
+
+		// add verifier
+		verifier = msg.sender;
+
+		if (approve_choice){
+			default_state = State.Verified;
+		} else {
+			default_state = State.Not_Verified;
+			handle_worker_failure();
 		}
 
-		verifiersList.push(msg.sender);
 		return true;
+	}
+
+	function handle_worker_failure() public returns(bool){
+		require(default_state == State.Not_Verified, "error: handle_worker_failure");
+
 	}
 
 	// transfer 
