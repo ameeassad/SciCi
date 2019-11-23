255a90ad42aa890a22a0d1b1da5c0d6ccc0174b5
Timmy Wu <timmy@stringbean.tech>
2019-11-22 23:35:11 -0500

    feat: almost fnished contract (?)

--- main/contracts/RWRC.sol
+++ main/contracts/RWRC.sol
@@ -1,64 +1,52 @@
 pragma solidity ^0.5.11;
+import "./USC.sol";
 
 contract RWRC{
+    address payable owner;
 
-	enum State {Unfunded, Unadopted, Adopted, Waiting_for_verifier, Verified, Not_Verified, Completed};
-	State default_state = State.Unfunded;
+	enum State {Unfunded, Unadopted, Adopted, Waiting_for_verifier, Verified, Not_Verified, Completed}
+	State public default_state = State.Unfunded;
 
-	address payable owner;
-	uint256 tid;
-	uint256 vrf_num = 0; 
-	uint256 req_stake;  // requester stake
-	uint256 wrk_stake;  // tasker stake 
-	uint256 vrf_stake;  // pool of verifier stake
-	uint256 vrf_ans = 0; // the number of verifier answered
-	uint256 set_pay;
-	address payable tasker;
-	string description;
-	bytes32 answer_hash;
-	uint deadline; 
+	uint256 public tid;
+	uint256 public vrf_num = 0; 
+	uint256 public req_stake;  // requester stake
+	uint256 public wrk_stake;  // tasker stake 
+	uint256 public vrf_stake;  // pool of verifier stake
+	uint256 public vrf_ans = 0; // the number of verifier answered
+	uint256 public set_pay;
+	address payable public tasker;
+	string public description;
+	bytes32 public answer_hash;
+	uint public deadline; 
+    Registration reg;
+	Verifier[3] public verifiers;
 
-	Verifier[] verifiers;
-	Registration reg;
 
+    //Removed public keyword in variabls below
 	struct Verifier{
-		public bool done;
-		public bool approved;
-		public address payable public_addr; 
+		bool done;
+		bool approved;
+		address payable public_addr; 
 	}
 
-	event Transfer(address _from, address _totasker, address _toverifier);
-	event Task_completed(address wrker, bytes32 answer);
+	//event Transfer(address _from, address _totasker, address _toverifier);
+	//event Task_completed(address wrker, bytes32 answer);
 
-	constructor(uint256 taskid, string memory des, address regis_addr, uint pay, uint duration) public {
-		tid = taskid;
+	constructor(string memory des, address regis_addr, uint pay, uint duration) public {
+	    reg = Registration(regis_addr);
+	    require(reg.isRequester(msg.sender), "You are not registered to be a requester");
+		tid = reg.addTaskCount();
 		owner = msg.sender;
 		description = des;
 		set_pay = pay;
-		deadline = now + duration; 
-
-		reg = Registration(regis_addr);
-		reg.addTaskCount():
-		depositFunds(); 
+		deadline = now + duration;
+		reg.addReq(owner);
 	}
 
-	// FALL BACK FUNCTION TODO
-	function () public payable {
-        depositFunds();
-    }
-
-    function depositFunds() payable {
-        req_stake += msg.value;
-
-		if (req_stake >= set_pay){
-			default_state = State.Unadopted;
-		}
-    }
-
 	// donors donate 
-	function donate() payable{
+	function donate() public payable{
 		req_stake += msg.value;
-
+		
 		if (req_stake >= set_pay){
 			default_state = State.Unadopted;
 		}
@@ -67,13 +55,15 @@ contract RWRC{
 
 	// tasker adopt a task
 	// TODO: interfacing with registration contract 
-	function tasker_adopt(uint256 stake) public payable returns(bool){
+	function tasker_adopt() public payable returns(bool){
+	    //check if taskers
+	    require(reg.isTasker(msg.sender), "You are not registered to be a tasker");
+	    require(owner != address(0x0), "error: This task is adopted by another tasker");
 		require(owner != msg.sender, "error: requester as tasker");
-		require(default_state == State.Unadopted, "error: the task is ready for adoption"):
+		require(default_state == State.Unadopted, "error: the task is not yet ready for adoption");
 
 		// setup tasker to task
 		tasker = msg.sender;
-		//TODO transfer stake from worker
 		wrk_stake += msg.value;
 		default_state = State.Adopted;
 		return true;
@@ -83,49 +73,76 @@ contract RWRC{
 	// tasker complete task 
 	function complete_task(string memory ans) public returns(bool) {
 		require(owner != msg.sender, "error: requester as tasker");
+		require(tasker == msg.sender, "error: wrong tasker completion");
+		require(default_state == State.Unadopted, "error: State error");
 
 		// tasker has completed task
 		default_state = State.Waiting_for_verifier;
-		answer_hash = keccak256(ans);
+		answer_hash = keccak256(bytes (ans));
 
 		return true;
 	}
 
-	function verifier_adopt(uint256 stake) public returns(bool) { 
+	function verifier_adopt() public payable returns(bool) {
+	    require(default_state == State.Waiting_for_verifier, "error: State error");
+	    require(vrf_num < 3, "error: State error");
+	    require(reg.isVerifier(msg.sender), "You are not registered to be a verifier");
 		require(tasker != msg.sender, "error: tasker as verifier");
+		require(msg.value > 0, "error: Verifier stake cannot be zero");
+		
+	    bool dup_verifier = false; 
+	    
+	    for (uint i = 0; i < vrf_num; i++){
+	        if (verifiers[i].public_addr == msg.sender) { 
+	            dup_verifier = true;
+	            break;
+	        }
+	    }
+
+	    require(!dup_verifier, "error: dupicate verifier");
 		
 		//setup verifier
-		if (vrf_num < 3){
-			verifiers[vrf_num] = new Verifier(false, false, msg.sender);
-			vrf_num++;
-			vrf_stake += msg.value;
-		}
+		Verifier memory v = Verifier(false, false, msg.sender); 
+	    verifiers[vrf_num] = v;
+		vrf_num ++;
+		vrf_stake += msg.value;
 	}
 
 	// Verifier does not approve the answer_hash
 	function verify_task(bool approve_choice) public returns(bool) {
-		require(msg.sender != tasker, "error: tasker as requester");
+	    require(vrf_num > 0, "errer: no verifier");
+		require(msg.sender != tasker, "error: tasker as verifier");
+		
+        bool verifier_exist = false; 
+	    
+	    for (uint i = 0; i < vrf_num; i++){
+	        if (verifiers[i].public_addr == msg.sender) { 
+	            verifier_exist = true;
+	            break;
+	        }
+	    }
+	    
+	    require(verifier_exist, "error: verifier does not exist");
 
-		// TODO: check reputation in registration contract 
-		vrf_ans ++; 
-
-		// change this mapping 
+		// change this mapping
+		reg.addVeri(msg.sender);
 		verifiers[vrf_ans].done = true; 
 		verifiers[vrf_ans].approved = approve_choice; 
+		vrf_ans ++; 
 
-		// Check if all verifier has answered, find concensus
+		// Check if all verifier has answered, find consensus
 		if (vrf_ans == 3){
 
-			bool concensus = find_consensus()
+			bool consensus = find_consensus();
 			address payable temp_addr = address(0x0);
 
-			for(int i = 0; i < vrf_num; i++){
-				if (verifiers[i].approved != concensus){
+			for(uint i = 0; i < vrf_num; i++){
+				if (verifiers[i].approved != consensus){
 					temp_addr = verifiers[i].public_addr;
 				}
 			}
 
-			if (concensus == false){
+			if (consensus == false){
 				if (temp_addr != address(0x0)){
 					dis_min(temp_addr);
 				} else {
@@ -146,21 +163,21 @@ contract RWRC{
 		return true;
 	}
 
-	function find_concensus() public returns(bool){
+	function find_consensus() public view returns(bool){
 
 		uint approved = 0; 
 		uint disapproved = 0;
 		bool result; 
 
-		for(int i = 0; i < vrf_num; i++){
-			if (verifers[i].approved == true){
+		for(uint i = 0; i < vrf_num; i++){
+			if (verifiers[i].approved == true){
 				approved++;
 			}else{
 				disapproved++;
 			}
 		}
 
-		if (approved > dispproved){
+		if (approved > disapproved){
 			result = true;
 		} else {
 			result = false; 
@@ -174,49 +191,51 @@ contract RWRC{
 		require(default_state == State.Not_Verified, "error: handle_tasker_failure");
 
 		// reset all variables
-		default_state = State.Unadopted; 
+		default_state = State.Unadopted;
 		wrk_stake = 0;
 		vrf_stake = 0;
 		vrf_num = 0;
 		vrf_ans = 0; 
 
-		reg.decreaseReputation(worker, 20, true);
-		worker = address(0x0);
+		reg.decreaseReputation(tasker, 20, true);
+		tasker = address(0x0);
 	}
 
-	function transfer_checked(address _to, uint256 _amount) public payable {
-		if (address(this).balance > _amount ) throw;
+	function transfer_checked(address payable _to, uint256 _amount) public payable {
+		if (address(this).balance < _amount ) revert();
         _to.transfer(_amount);
     }
 
 	// approved, all app
 	// calculation for all the transfers 
-	function app_all() private payable{
+	function app_all() private {
 		// stake refund back
 		transfer_checked(tasker, req_stake*7/10);
 		uint256 vrf_pay = req_stake/10 + vrf_stake/3;
-		for(int i = 0; i < vrf_num; i++){
+		for(uint i = 0; i < vrf_num; i++){
 			transfer_checked(verifiers[i].public_addr, vrf_pay);
 			reg.increaseReputation(verifiers[i].public_addr, 20, false);	
 		}
 	}
 
-	function app_min(address min) private payable{
+	function app_min(address min) private {
         uint256 temp = vrf_stake/2;
         uint256 comb_stake = (req_stake/10) + temp;
+        
         for (uint i = 0; i < vrf_num; i++){
+            
             if ((verifiers[i]).public_addr != min){
                 transfer_checked((verifiers[i]).public_addr, comb_stake);
 				reg.increaseReputation(verifiers[i].public_addr, 20, false);
             } else {
 				reg.decreaseReputation(verifiers[i].public_addr, 20, true);		
 			}
+			
         }
-
-		transfer_checked(tasker, req_stake*8/10);
+		transfer_checked(tasker, req_stake*8/10 + wrk_stake);
     }
 
-	function dis_min(address min) private payable{
+	function dis_min(address min) private {
 		// workers stake + vrf_stake 
 		uint256 temp = wrk_stake + vrf_stake;
 		for(uint256 i = 0; i < vrf_num; i++){
@@ -241,10 +260,13 @@ contract RWRC{
 	   
 	    if ( now > deadline ){
 	       owner.transfer(req_stake);
-		   selfdestruct(owner); 
+		   selfdestruct(owner);
 	    }
+	}
+	
+	function getContractBalance() public view returns(uint){
+	    return address(this).balance;
 	}
 
 
 }
-
--- main/contracts/USC.sol
+++ main/contracts/USC.sol
@@ -16,23 +16,16 @@ contract Registration{
     // list of tasks
 
     mapping(address => Actor) public actors;
-    mapping(address => Donor) public donors;
     mapping(address => Tasker) public taskers;
     mapping(address => Requester) public requesters;
     mapping(address => Verifier) public verifiers;
 
     struct Actor {
         address payable public_addr;
-        bool donor;
         bool requester;
         bool tasker;
         bool verifier;
         uint reputation;
-    }
-
-    struct Donor {
-        address payable public_addr;
-        uint donation_count;
     }
 
     struct Tasker {
@@ -62,29 +55,11 @@ contract Registration{
         wallet = msg.sender;
     }
 
-    function registerDonor() public {
-        require(donors[msg.sender].public_addr == address(0x0), "Duplicate registration");
-
-        if (actors[msg.sender].public_addr == address(0x0)){
-            actors[msg.sender] = Actor(msg.sender, false, true, false, false, 100);
-            actorCount += 1;
-
-            donors[msg.sender] = Donor(msg.sender, 0);
-            donorCount += 1;
-        }else{
-            if (donors[msg.sender].public_addr == address(0x0)){
-                donors[msg.sender] = Donor(msg.sender, 0);
-                donorCount += 1;
-            }
-            actors[msg.sender].donor = true;
-        }
-    }
-
     function registerTasker() public {
         require(taskers[msg.sender].public_addr == address(0x0), "Duplicate registration");
 
         if (actors[msg.sender].public_addr == address(0x0)){
-            actors[msg.sender] = Actor(msg.sender, false, true, false, false, 100);
+            actors[msg.sender] = Actor(msg.sender, false, true, false, 100);
             actorCount += 1;
 
             taskers[msg.sender] = Tasker(msg.sender, 0, 0);
@@ -103,7 +78,7 @@ contract Registration{
         require(requesters[msg.sender].public_addr == address(0x0), "Duplicate registration");
 
         if (actors[msg.sender].public_addr == address(0x0)){
-            actors[msg.sender] = Actor(msg.sender, false, true, false, false, 100);
+            actors[msg.sender] = Actor(msg.sender, true, false, false, 100);
             actorCount += 1;
 
             requesters[msg.sender] = Requester(msg.sender, 0);
@@ -126,34 +101,15 @@ contract Registration{
     }
 
     function removeVerifier(address addr) public {
-        require(verifiers[addr].public_addr != address(0x0), "Duplicate registration");
+        require(verifiers[addr].public_addr != address(0x0), "Duplicate remove registration");
         actors[addr].verifier = false;
+        verifierCount--; 
         delete verifiers[addr];
     }
 
     function getRep(address addr) public view returns (uint256){
         require(actors[addr].public_addr != address(0x0), "Address does not exist");
         return actors[addr].reputation;
-    }
-
-    function getActorCount() public view returns(uint256){
-        return actorCount;
-    }
-
-    function getTaskerCount() public view returns(uint256){
-        return taskerCount;
-    }
-
-    function getRequesterCount() public view returns(uint256){
-        return requesterCount;
-    }
-
-    function getVerifierCount() public view returns(uint256){
-        return verifierCount;
-    }
-
-    function getTransactionCount() public view returns(uint256){
-        return transactionCount;
     }
 
     function addTaskCount() public returns(uint256){
@@ -163,25 +119,27 @@ contract Registration{
 
     //increase reputation by inc if task is approved
     //value of inc depends on the level of the task
-    function increaseReputation(address addr, uint inc, bool isWorker) public{
-        Actor memory actor = actors[addr];
-        actor.reputation += inc;
+    function increaseReputation(address addr, uint inc, bool isWorker) public returns(uint256){
+        actors[addr].reputation += inc;
 
         //kind equals 0 if addr belongs to tasker/worker
-        if ( actor.verifier == false && isWorker && actor.reputation > threshold){
+        if ( actors[addr].verifier == false && isWorker && actors[addr].reputation > threshold){
             registerVerifier(addr);
         }
+        
+        return actors[addr].reputation;
     }
 
     //decrease reputation by inc if task is rejected
     //value of dec depends on the level of the task
-    function decreaseReputation(address addr, uint dec, bool isVerifier) public{
-        Actor memory actor = actors[addr];
-        actor.reputation -= dec;
+    function decreaseReputation(address addr, uint dec, bool isVerifier) public returns(uint256){
+        actors[addr].reputation -= dec;
 
-        if ( isVerifier && actor.reputation < threshold){
+        if ( isVerifier && actors[addr].reputation < threshold){
             removeVerifier(addr);
         }
+        
+        return actors[addr].reputation;
     }
 
     //track number of tasks worker has completed
@@ -190,9 +148,31 @@ contract Registration{
         tasker.num_job_done += 1;
     }
 
-    function taskFailed(address addr) public{
+    function taskFailed(address addr) public {
         Tasker storage tasker = taskers[addr];
         tasker.num_job_fail += 1;
     }
-
+    
+    function addReq(address addr) public {
+        Requester storage req = requesters[addr];
+        req.num_req += 1;
+    }
+    
+    function addVeri(address addr) public {
+        Verifier storage veri = verifiers[addr];
+        veri.num_veri += 1;
+    }
+    
+    function isTasker(address addr) public view returns(bool) {
+        return actors[addr].tasker;
+    }
+    
+    function isRequester(address addr) public view returns(bool) {
+        return actors[addr].requester;
+    }
+    
+    function isVerifier(address addr) public view returns(bool) {
+        return actors[addr].verifier;
+    }
+    
 }
